d51fcc5b6429c1240b55723759973c0348cd819d
Wael HAZAMI <whazami@e1r8p18.clusters.42paris.fr>
2022-11-26 20:00:26 +0100

    post body added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,15 +7,20 @@ import './App.css';
 // }
 
 function App() {
+  let [id, updateId]: any = useState(null); // useReducer
+
   fetch("http://127.0.0.1:3001/user/profile", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({id: "1"})
+    body: JSON.stringify({id: "3"})
   })
     .then(response => response.json())
-    .then(data => console.log("id: ===> ", data.id, "name ===>", data.name, "avatar ==>", data.avatar))
+    .then(data => {
+      console.log("id: ===> ", data.id, "name ===>", data.name, "avatar ==>", data.avatar);
+      updateId(data.id);
+    })
     .catch(error => console.error("error ==> ", error));
 
   return (
@@ -23,7 +28,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. id: {id}
         </p>
         <a
           className="App-link"
