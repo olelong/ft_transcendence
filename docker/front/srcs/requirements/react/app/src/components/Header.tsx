cd7505cfa674479a82d0aa46776de7a73ad6270a
Wael HAZAMI <whazami@e2r1p4.clusters.42paris.fr>
2023-01-07 17:32:51 +0100

    42 api finished but tokens on localStorage, not in cookies

--- docker/front/srcs/requirements/react/app/src/components/Header.tsx
+++ docker/front/srcs/requirements/react/app/src/components/Header.tsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export default function Header() {
-  const [login, setLogin] = useState("");
+  const [login, setLogin] = useState(
+    localStorage.getItem("catpong") &&
+      JSON.parse(localStorage.getItem("catpong") || "[]").login
+  );
 
   useEffect(() => {
-    if (
-      window.location.href.startsWith(process.env.REACT_APP_REDIRECT_URI || "")
-    ) {
+    if (!login) {
       let params = new URL(window.location.href).searchParams;
       let code = params.get("code");
       fetch("https://api.intra.42.fr/oauth/token", {
@@ -41,7 +42,7 @@ export default function Header() {
           else console.error(err);
         });
     }
-  }, []);
+  }, [login]);
 
   return (
     <div>
@@ -66,35 +67,39 @@ function getUserInfos(setLogin: React.Dispatch<React.SetStateAction<string>>) {
     .then((res) => {
       if (res.status === 200) return res.json();
       else if (res.status === 401) {
-        fetch("https://api.intra.42.fr/oauth/token", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            client_id: process.env.REACT_APP_UID,
-            client_secret: process.env.REACT_APP_SECRET,
-            refresh_token: refresh_token,
-            redirect_uri: process.env.REACT_APP_REDIRECT_URI,
-            grant_type: "refresh_token",
-          }),
-        })
-          .then((res) => res.json())
-          .then((access_token) => {
-            localStorage.setItem(
-              "catpong",
-              JSON.stringify({ tokens: { access_token, refresh_token } })
-            );
-            getUserInfos(setLogin);
-          })
-          .catch((err) => console.error(err));
+        refreshToken(refresh_token, () => getUserInfos(setLogin));
         throw new Error("ignore");
       }
     })
     .then((data) => {
-      setLogin(data.login);
+      if (data) setLogin(data.login);
     })
     .catch((err) => {
       if (err.message !== "ignore") console.error(err);
     });
+}
+
+function refreshToken(refresh_token: string, end_function: () => void): void {
+  fetch("https://api.intra.42.fr/oauth/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      client_id: process.env.REACT_APP_UID,
+      client_secret: process.env.REACT_APP_SECRET,
+      refresh_token: refresh_token,
+      redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+      grant_type: "refresh_token",
+    }),
+  })
+    .then((res) => res.json())
+    .then((access_token) => {
+      localStorage.setItem(
+        "catpong",
+        JSON.stringify({ tokens: { access_token, refresh_token } })
+      );
+      end_function();
+    })
+    .catch((err) => console.error(err));
 }
