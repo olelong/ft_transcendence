62e93183440f2b4fd298ec6569291f3f34f6b094
Yoojeong TAK <ytak@e2r2p3.clusters.42paris.fr>
2023-01-07 17:56:33 +0100

    some bootstrap

--- docker/front/srcs/requirements/react/app/src/pages/Login.tsx
+++ docker/front/srcs/requirements/react/app/src/pages/Login.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
+import { Image, Row, Col, Button, Container } from "react-bootstrap";
 
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "../styles/Login.css";
 import logo from "../assets/main/logoWithoutBg.png";
+
+console.log("salut");
 
 export default function Login() {
   const [msg, updateMsg] = useState();
@@ -18,16 +17,16 @@ export default function Login() {
 
   return (
     <Container>
-      {/** <p>msg from server: {msg}</p> */}
-      <Row>
-        <div className="bg-image d-flex justify-content-center align-items-center">
-          {/**Card  */}
-          <div>
-            <img src={logo} alt="logo" />
-            <br />
-            <Button className="btn-outline-light btn-lg my-btn">Login</Button>
-          </div>
-        </div>
+      <Row className="align-items-center">
+        <Col xs={12} md={8}>
+          <Image
+            className="myImg"
+            src={logo}
+            alt="Logo image"
+            fluid
+          />
+          <Button className="btn-outline-light btn-lg my-btn">Login</Button>
+        </Col>
       </Row>
     </Container>
   );
