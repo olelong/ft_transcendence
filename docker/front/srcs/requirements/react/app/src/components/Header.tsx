75c90510c1b371a0455948a09b5f8b6260b7fdf2
olelong <olelong@student.42.fr>
2023-01-11 11:49:49 +0100

    avance sur le header

--- docker/front/srcs/requirements/react/app/src/components/Header.tsx
+++ docker/front/srcs/requirements/react/app/src/components/Header.tsx
@@ -1,14 +1,25 @@
 import { Link, Outlet } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/header.css'
 
+import logo from '../assets/main/pictoGrand.png';
+
+
 export default function Header() {
   return (
-    <div>
-      <h1 className="logoName" >CATPONG</h1>
-      <Link to="/home/profile">Profile</Link>
-      <Link to="/home/play">Play</Link>
-      <Link to="/home/chat">Chat</Link>
+    <div className="header">
+      <Navbar>
+      <Navbar.Brand href="/home/play" className="logo">
+        <img src={logo} alt="CatPong's logo" width="30"/>
+        <h1 className="logoName" >CATPONG</h1>
+      </Navbar.Brand>
+      </Navbar>
+      <Navbar className="nav">
+      <Link className="profile" to="/home/profile">Profile</Link>
+      <Link className="play" to="/home/play">Play</Link>
+      <Link className="chat" to="/home/chat">Chat</Link>
+      </Navbar>
       <Outlet />
     </div>
   );
