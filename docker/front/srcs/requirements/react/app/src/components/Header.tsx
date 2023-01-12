6383ae174480faf6845b31dc1bcaa805800ffe26
olelong <olelong@student.42.fr>
2023-01-12 12:11:52 +0100

    nav OK

--- docker/front/srcs/requirements/react/app/src/components/Header.tsx
+++ docker/front/srcs/requirements/react/app/src/components/Header.tsx
@@ -1,44 +1,33 @@
-import { Link, Outlet } from "react-router-dom";
-import Navbar from 'react-bootstrap/Navbar';
+import { Outlet } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/header.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/header.css";
 
-import logo from '../assets/main/pictoGrand.png';
-import { Nav } from "react-bootstrap";
-
+import logo from "../assets/main/pictoGrand.png";
 
 export default function Header() {
   return (
     <div className="header">
       <Navbar>
-      <Navbar.Brand href="/home/play" className="logo">
-        <img src={logo} alt="CatPong's logo" width="30"/>
-        <h1 className="logoName" >CATPONG</h1>
-      </Navbar.Brand>
+        <Navbar.Brand href="/home/play" className="logo">
+          <img src={logo} alt="CatPong's logo" width="30" />
+          <h1 className="logoName">CATPONG</h1>
+        </Navbar.Brand>
       </Navbar>
-      <Navbar className="nav">
-      <LinkContainer to="/home/profile" activeClassName="active-profile">
-        <Nav.Link className="profile"> Profile</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/home/play" activeClassName="active-play">
-        <Nav.Link className="play"> Play</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/home/chat" activeClassName="active-chat">
-        <Nav.Link className="chat"> Chat</Nav.Link>
-      </LinkContainer>
-      </Navbar>
+      <Nav className="nav">
+        <LinkContainer to="/home/profile" activeClassName="active-nav">
+          <Nav.Link className="profile">Profile</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/home/play" activeClassName="active-nav">
+          <Nav.Link className="play">Play</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/home/chat" activeClassName="active-nav">
+          <Nav.Link className="chat">Chat</Nav.Link>
+        </LinkContainer>
+      </Nav>
       <Outlet />
     </div>
   );
 }
-
-//<LinkContainer to="/home/profile" activeClassName="active-profile">
-//<Nav.Link className="profile"> Profile</Nav.Link>
-//</LinkContainer>
-//<LinkContainer to="/home/play" activeClassName="active-play">
-//<Nav.Link className="play"> Play</Nav.Link>
-//</LinkContainer>
-//<LinkContainer to="/home/chat" activeClassName="active-chat">
-//<Nav.Link className="chat"> Chat</Nav.Link>
-//</LinkContainer>
