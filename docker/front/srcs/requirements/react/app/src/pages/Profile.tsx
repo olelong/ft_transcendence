28be15abc22b054ad3b4c7cbcf6b79b25c475deb
olelong <olelong@student.42.fr>
2023-01-17 17:17:19 +0100

    reflexion profile

--- docker/front/srcs/requirements/react/app/src/pages/Profile.tsx
+++ docker/front/srcs/requirements/react/app/src/pages/Profile.tsx
@@ -3,7 +3,36 @@ export default function Profile() {
   }
 /*
 
-- avatar photo avec possibilité de  
+- Avatar photo avec possibilité de le changer:
+  Donc un composant pour modifier l'avatar.
+- Container => id, displayName, 2FA, STATS, theme:
+  Donc un composant, pour changer le displayName.
+  Un autre pour activer/désactiver la 2FA.
+  Un autre pour mettre a jour les stats automatiquement.
+  Un autre pour changer le thème d'une game.
+- Container => Contenant tous les achievements:
+  Un composant pour la liste entière des achievements et
+  un autre pour s'occuper de chaque achievements individuellement.
+  Gérer la liste de tous les achievements et mettre a jour
+  ceux qui sont atteints. 
+- Ajouter la possibilité de voir la liste des amis bloqués pour en 
+débloquer ou non.
 
+
+Si il y a pas d'avatar choisi, en mettre un par defaut.
 */
-  
+
+/*ACHIEVEMENTS*/
+/*
+  - Win more than 10 games
+  - Create a channel group
+  - Win your first game
+  - Win more than 50 games
+  - Add a first friend
+  - Play with another theme
+  - Become Rank 1
+  - Be in the top 3
+  - Play for the first time
+
+  => Trouver des noms sympas, des pictos d'achievements.
+*/
