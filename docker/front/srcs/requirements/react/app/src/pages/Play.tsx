ba6fc84c9c3327f210637c9571c69acbe7df3869
Yoojeong TAK <ytak@e2r2p20.clusters.42paris.fr>
2023-02-04 18:34:01 +0100

    style css

--- docker/front/srcs/requirements/react/app/src/pages/Play.tsx
+++ docker/front/srcs/requirements/react/app/src/pages/Play.tsx
@@ -6,7 +6,8 @@ import { serverUrl } from "index";
 import EyeImg from "../assets/icons/eye2.png";
 import trophyImg from "../assets/podium/trophee.png";
 import xImg from "../assets/icons/eye2.png";
-import { useEffect, useState } from "react";
+import { RxCross2 } from "react-icons/rx";
+import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 export const LS_KEY_42API = "42-tokens";
 
@@ -17,10 +18,12 @@ export default function Play() {
   const [showDiv, setShowDiv] = useState(false);
   const [winnerAvatar, setWinnerAvatar] = useState([]);
 
+  // Manage horizontal scroll
+  const scrollContainer = useRef<HTMLDivElement>(null);
+
   const handleButtonClick = () => {
     setShowDiv(!showDiv);
-  }
-
+  };
 
   useEffect(() => {
     fetch(serverUrl + "game/friendsplaying", { credentials: "include" })
@@ -63,83 +66,97 @@ export default function Play() {
           >
             PLAY
           </Button>
-
-          {friendsPlaying.length > 0 && (
-            <h3 className="friends-title">Friends playing</h3>
-          )}
         </Col>
         {/* Second col to display the friends */}
-        {friendsPlaying.length > 0 && (
-          <Row className="friends-row" xs={12} md={12}>
-            {/* loop for display the users */}
-            {friendsPlaying.map((eachFriend: UserInfosProvider) => {
-              return (
-                <Col
-                  className="friends-col"
-                  key={eachFriend.id}
-                  xs={12}
-                  md={4}
-                  lg={2}
-                >
-                  <Link to={"/home/game/" + eachFriend.gameid}>
-                    <Image
-                      className="eyes"
-                      src={EyeImg}
-                      alt="eye-image"
-                      fluid
-                    />
-                  </Link>
-                  <Image
-                    className="gamers-img"
-                    src={eachFriend.avatar}
-                    alt="User image"
-                  />
-                  {/** */}
-                </Col>
-              );
-            })}
-          </Row>
-        )}
-      </Row>
-      
-      {/* For leaderboard, trophy */}
-      <Row className="trophy-col" xs={12} md={12}>
-        <Col className="trophy-column" xs={12} md={4} lg={2}>
-          <button
-            className="trophy-button"
-            onClick={handleButtonClick} >
-            {showDiv ? ( showDiv &&<img src={xImg} className="x-img" />
-): ( <img src={trophyImg} className="trophy-img" />) }
-    
-          </button>
-          {showDiv  && (
-            <div className="showDiv">
-              <h2 className="podium-title">Leaderboard</h2>
-              <Row>
-                {/* display the winners */}
-                {winnerAvatar.map((eachWinner: UserInfosProvider, i) => {
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {friendsPlaying.length > 0 && (
+              <h3 className="friends-title">Friends playing</h3>
+            )}
+            {friendsPlaying.length > 0 && (
+              <Row
+                className="friends-row"
+                xs={12}
+                md={12}
+                ref={scrollContainer}
+                onWheel={(e) => {
+                  e.preventDefault();
+                  if (scrollContainer.current)
+                    scrollContainer.current.scrollLeft += e.deltaY;
+                }}
+                style={{
+                  paddingLeft: 30,
+                  flexDirection: friendsPlaying.length < 6 ? "row" : "column",
+                  overflowX: friendsPlaying.length < 6 ? "hidden" : "scroll",
+                  height: friendsPlaying.length < 6 ? 170 : 180,
+                }}
+              >
+                {/* loop for display the users */}
+                {friendsPlaying.map((eachFriend: UserInfosProvider) => {
                   return (
-                    <Col key={eachWinner.id} xs={12} md={4} lg={2}>
-                      <div className={`winner-col winner-col-${i}`}>
+                    <div className="friend-image-container">
+                      <Image
+                        className="gamers-img"
+                        src={eachFriend.avatar}
+                        alt="User image"
+                      />
+                      <Link
+                        to={"/home/game/" + eachFriend.gameid}
+                        style={{ padding: 0 }}
+                      >
                         <Image
-                          onClick={() =>
-                            (window.location.href = `/home/profile/${eachWinner.id}`)
-                          }
-                          className="winners-img"
-                          src={eachWinner.avatar}
-                          alt="winner image"
+                          className="eyes"
+                          src={EyeImg}
+                          alt="eye-image"
                           fluid
                         />
-                      </div>
-
-                      {/** */}
-                    </Col>
+                      </Link>
+                    </div>
                   );
                 })}
               </Row>
-            </div>
-          )}
-        </Col>
+            )}
+          </div>
+          {/* For leaderboard, trophy */}
+          <Row className="trophy-col" xs={12} md={12}>
+            <Col className="trophy-column" xs={12} md={4} lg={2}>
+              <button className="trophy-button" onClick={handleButtonClick}>
+                {showDiv ? (
+                  showDiv && <RxCross2 size={60} className="x-img"/>
+                ) : (
+                  <img src={trophyImg} className="trophy-img" />
+                )}
+              </button>
+              {showDiv && (
+                <div className="showDiv">
+                  <h2 className="podium-title">Leaderboard</h2>
+                  <Row>
+                    {/* display the winners */}
+                    {winnerAvatar.map((eachWinner: UserInfosProvider, i) => {
+                      return (
+                        <Col key={eachWinner.id} xs={12} md={4} lg={2}>
+                          <div className={`winner-col winner-col-${i}`}>
+                            <Image
+                              onClick={() =>
+                                (window.location.href = `/home/profile/${eachWinner.id}`)
+                              }
+                              className="winners-img"
+                              src={eachWinner.avatar}
+                              alt="winner image"
+                              fluid
+                            />
+                          </div>
+
+                          {/** */}
+                        </Col>
+                      );
+                    })}
+                  </Row>
+                </div>
+              )}
+            </Col>
+          </Row>
+        </div>
       </Row>
     </Container>
   );
