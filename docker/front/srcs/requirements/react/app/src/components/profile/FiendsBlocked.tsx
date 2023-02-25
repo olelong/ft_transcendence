acfb05cc8b771f134c789fa7a622ee24795974d6
Oriane LELONG <olelong@e1r8p12.clusters.42paris.fr>
2023-02-25 18:06:34 +0100

    add scrollbar

--- docker/front/srcs/requirements/react/app/src/components/profile/FiendsBlocked.tsx
+++ docker/front/srcs/requirements/react/app/src/components/profile/FiendsBlocked.tsx
@@ -4,7 +4,7 @@ import star from "../../assets/icons/star.png";
 
 export default function FriendsBlocked() {
   return (
-    <>
+    <div className="user-blocked-global">
     <div className="user-blocked-div">
       <img src={star} alt='avatar of a user blocked' className="user-blocked-avatar" style={{width: "40px", borderRadius:"100%"}}/>
       <p className="user-blocked-p">name</p>
@@ -25,7 +25,37 @@ export default function FriendsBlocked() {
       <p className="user-blocked-p">name2222222</p>
       <button className="user-unblocked-button">Unblock</button>
     </div>
-    </>
+    <div className="user-blocked-div">
+      <img src={star} alt='avatar of a user blocked' className="user-blocked-avatar" style={{width: "40px", borderRadius:"100%"}}/>
+      <p className="user-blocked-p">name</p>
+      <button className="user-unblocked-button">Unblock</button>
+    </div>
+    <div className="user-blocked-div">
+      <img src={star} alt='avatar of a user blocked' style={{width: "40px"}}/>
+      <p className="user-blocked-p">123456789012345678901234567890</p>
+      <button className="user-unblocked-button">Unblock</button>
+    </div>
+    <div className="user-blocked-div">
+      <img src={star} alt='avatar of a user blocked' style={{width: "40px"}}/>
+      <p className="user-blocked-p">12345678901234</p>
+      <button className="user-unblocked-button">Unblock</button>
+    </div>
+    <div className="user-blocked-div">
+      <img src={star} alt='avatar of a user blocked' style={{width: "40px"}}/>
+      <p className="user-blocked-p">name2222222</p>
+      <button className="user-unblocked-button">Unblock</button>
+    </div>
+        <div className="user-blocked-div">
+      <img src={star} alt='avatar of a user blocked' style={{width: "40px"}}/>
+      <p className="user-blocked-p">12345678901234</p>
+      <button className="user-unblocked-button">Unblock</button>
+    </div>
+    <div className="user-blocked-div">
+      <img src={star} alt='avatar of a user blocked' style={{width: "40px"}}/>
+      <p className="user-blocked-p">name2222222</p>
+      <button className="user-unblocked-button">Unblock</button>
+    </div>
+    </div>
   );
 
 }
