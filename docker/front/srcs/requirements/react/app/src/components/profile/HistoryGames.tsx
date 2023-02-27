f2346af99a1de2aaae83036915e153582fa48d6b
olelong <olelong@student.42.fr>
2023-02-27 22:06:08 +0100

    style history game in process

--- docker/front/srcs/requirements/react/app/src/components/profile/HistoryGames.tsx
+++ docker/front/srcs/requirements/react/app/src/components/profile/HistoryGames.tsx
@@ -6,6 +6,7 @@ import win from "../../assets/history/win.png";
 import lose from "../../assets/history/lose.png";
 import titleHistory from "../../assets/history/titleHistory.png";
 import { Games } from "../../types/profile.interface";
+import { userInfo } from "os";
 
 export default function HistoryGames({
   userInfosGames,
@@ -20,23 +21,29 @@ export default function HistoryGames({
             src={titleHistory}
             alt="title's icon for history"
             className="title-history-icon-left"
-            style={{ width: "20px", transform: "scaleX(-1)" }}
           />
           <p className="title-history-date">20/12/2012, 03:00:00</p>
           <img
             src={titleHistory}
             alt="title's icon for history"
             className="title-history-icon-right"
-            style={{ width: "20px" }}
           />
         </div>
-        <div className="history-games-score">
-        <img src={win} alt="win icon" className="win-icon" />
-        <p>0</p>
-        <p>me</p>
-        <img src={lose} alt="lose icon" style={{ width: "60px" }} />
-        <p>11</p>
-        <p>enemy</p>
+        <div className="history-games-myscore">
+          <img
+            src={
+              /*userInfosGames.myScore > userInfosGames.enemyScore ? win : lose*/ win
+            }
+            alt="myscore icon"
+            style={{ width: "45px" }}
+          />
+          <p className="score-p">0</p>
+          <p className="history-game-name">me</p>
+        </div>
+        <div className="history-games-enemy-score">
+          <img src={lose} alt="enemy score icon" style={{ width: "45px" }} />
+          <p className="score-p">11</p>
+          <p>enemy</p>
         </div>
       </div>
     </>
