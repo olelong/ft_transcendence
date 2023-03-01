7eed317b98e675b5c98dff4e6880f4116ea822ba
olelong <olelong@student.42.fr>
2023-03-01 23:15:23 +0100

    ajout du map

--- docker/front/srcs/requirements/react/app/src/components/profile/Achievements.tsx
+++ docker/front/srcs/requirements/react/app/src/components/profile/Achievements.tsx
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { AchievementsProps } from "../../types/profile.interface";
 import "../../styles/profile/ProfileTabs.css";
 
-import badge from "../../assets/achievements/badge1.png";
 import ProgressBar from "react-bootstrap/ProgressBar";
 
 export default function Achievements({
@@ -10,6 +9,7 @@ export default function Achievements({
 }: {
   userInfosAchievements: AchievementsProps[];
 }) {
+  console.log("ok: ", userInfosAchievements[0].score);
   return (
     <div
       className="achiev-global"
@@ -20,126 +20,33 @@ export default function Achievements({
             : "hidden",
       }}
     >
-      <div className="achiev-div">
-        <div className="achiev-left">
-          <p className="achiev-p">Win 5 games</p>
-          <p className="achiev-p" style={{ fontSize: "13px" }}>
-            Description
-          </p>
-          <ProgressBar
-            className="achiev-progress-bar"
-            animated
-            variant="achiev-progress-bar"
-            label="2 / 5 games"
-            now={2}
-            min={0}
-            max={5}
-          />
-        </div>
-        <div className="achiev-right">
-          <img
-            src={badge}
-            alt="achievements's icon"
-            style={{ width: "100%" }}
-          />
-        </div>
-      </div>
-      <div className="achiev-div">
-        <div className="achiev-left">
-          <p className="achiev-p">Win 5 games</p>
-          <p className="achiev-p" style={{ fontSize: "13px" }}>
-            Description
-          </p>
-          <ProgressBar
-            className="achiev-progress-bar"
-            animated
-            variant="achiev-progress-bar"
-            label="2 / 5 games"
-            now={2}
-            min={0}
-            max={5}
-          />
-        </div>
-        <div className="achiev-right">
-          <img
-            src={badge}
-            alt="achievements's icon"
-            style={{ width: "100%" }}
-          />
-        </div>
-      </div>
-      <div className="achiev-div">
-        <div className="achiev-left">
-          <p className="achiev-p">Win 5 games</p>
-          <p className="achiev-p" style={{ fontSize: "13px" }}>
-            Description
-          </p>
-          <ProgressBar
-            className="achiev-progress-bar"
-            animated
-            variant="achiev-progress-bar"
-            label="2 / 5 games"
-            now={2}
-            min={0}
-            max={5}
-          />
-        </div>
-        <div className="achiev-right">
-          <img
-            src={badge}
-            alt="achievements's icon"
-            style={{ width: "100%" }}
-          />
-        </div>
-      </div>
-      <div className="achiev-div">
-        <div className="achiev-left">
-          <p className="achiev-p">Win 5 games</p>
-          <p className="achiev-p" style={{ fontSize: "13px" }}>
-            Description
-          </p>
-          <ProgressBar
-            className="achiev-progress-bar"
-            animated
-            variant="achiev-progress-bar"
-            label="2 / 5 games"
-            now={2}
-            min={0}
-            max={5}
-          />
-        </div>
-        <div className="achiev-right">
-          <img
-            src={badge}
-            alt="achievements's icon"
-            style={{ width: "100%" }}
-          />
-        </div>
-      </div>
-      <div className="achiev-div">
-        <div className="achiev-left">
-          <p className="achiev-p">Win 5 games</p>
-          <p className="achiev-p" style={{ fontSize: "13px" }}>
-            Description
-          </p>
-          <ProgressBar
-            className="achiev-progress-bar"
-            animated
-            variant="achiev-progress-bar"
-            label="2 / 5 games"
-            now={2}
-            min={0}
-            max={5}
-          />
-        </div>
-        <div className="achiev-right">
-          <img
-            src={badge}
-            alt="achievements's icon"
-            style={{ width: "100%" }}
-          />
-        </div>
-      </div>
+      {userInfosAchievements &&
+        userInfosAchievements.map((achiev: any, index) => (
+          <div className="achiev-div" key={index}>
+            <div className="achiev-left">
+              <p className="achiev-p">{achiev.name}</p>
+              <p className="achiev-p" style={{ fontSize: "13px" }}>
+                {achiev.desc}
+              </p>
+              <ProgressBar
+                className="achiev-progress-bar"
+                animated
+                variant="achiev-progress-bar"
+                label={`${achiev.score} / ${achiev.goal} games`}
+                now={achiev.score}
+                min={0}
+                max={achiev.goal}
+              />
+            </div>
+            <div className="achiev-right">
+              <img
+                src={achiev.img}
+                alt="achievements's icon"
+                style={{ width: "100%" }}
+              />
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
