a6cfba03c14d8ebdaea46cfe60027ecb6888a636
wael <wl.hazami@gmail.com>
2023-04-11 02:01:10 +0200

    fetch for get friends/pending/channels list

--- docker/fullStack/srcs/requirements/react/app/src/components/Chat/Left.tsx
+++ docker/fullStack/srcs/requirements/react/app/src/components/Chat/Left.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import Button from "react-bootstrap/Button";
 
@@ -8,9 +8,85 @@ import "../../styles/Chat/containers.css";
 import "../../styles/Chat/Left.css";
 
 import CatPongImage from "../CatPongImage";
+import { ShowStatus } from "./Right/MembersCategory";
+
+import { serverUrl } from "index";
 
 export default function Left() {
   const { setCurrConv } = useContext(ConvContext);
+
+  const [pendings, setPendings] = useState<Member[]>();
+  const [friends, setFriends] = useState<Member[]>();
+  const [channels, setChannels] = useState<[]>();
+
+  const [pendingsStatus, setPendingsStatus] = useState<{
+    status?: string;
+    gameid?: string;
+  }>();
+  const [friendsStatus, setFriendsStatus] = useState<{
+    status?: string;
+    gameid?: string;
+  }>();
+
+  // Get all friends and pending list
+  useEffect(() => {
+    fetch(serverUrl + "/user/friends/", {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setFriends(data.friends);
+        setPendings(data.pendings);
+      })
+      .catch((err) => console.error(err));
+  }, [pendings, friends]);
+
+    // Get user's channels list
+    useEffect(() => {
+      fetch(serverUrl + "/chat/channels/", {
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setChannels(data.channels);
+        })
+        .catch((err) => console.error(err));
+    }, [channels]);
+
+  pendings &&
+    pendings.map((pending) => (
+      <Button
+        className="left-avatar-button"
+        onClick={() =>
+          setCurrConv({
+            isChan: false,
+            id: pending.id,
+            name: pending.name,
+            avatar: pending.avatar,
+          })
+        }
+      >
+        <CatPongImage user={pending} className="left-avatar" />
+        <ShowStatus
+          member={{ status: "online" }}
+          styleOnOffline={{
+            position: "absolute",
+            top: "70%",
+            left: "calc(50% + 12px)",
+            width: "18%",
+            height: "20%",
+          }}
+          styleInGame={{
+            position: "absolute",
+            top: "70%",
+            left: "calc(50% + 12px)",
+            width: "18%",
+            height: "20%",
+          }}
+        />
+      </Button>
+    ));
+
   return (
     <div id="chat-left" className="purple-container">
       {/* PENDING PART */}
@@ -27,10 +103,27 @@ export default function Left() {
         }
       >
         <CatPongImage
-          user={{ id: "$test", name: "test", avatar: "/image/default.jpg" }}
+          user={{ id: "$test2", name: "test2", avatar: "/image/default.jpg" }}
           className="left-avatar"
         />
-        <div className="left-status" />
+        <ShowStatus
+          member={{}}
+          styleOnOffline={{
+            position: "absolute",
+            top: "70%",
+            left: "calc(50% + 12px)",
+            width: "18%",
+            height: "20%",
+          }}
+          styleInGame={{
+            position: "absolute",
+            top: "70%",
+            left: "calc(50% + 12px)",
+            width: "18%",
+            height: "20%",
+          }}
+        />
+        {/*<div className="left-status" />*/}
       </Button>
 
       {/* FRIENDS PART */}
@@ -53,15 +146,13 @@ export default function Left() {
         <div className="left-status" />
       </Button>
 
-      {/* CHANNELS PART */}
-      <p className="left-title">Channels</p>
       <Button
         onClick={() =>
           setCurrConv({
-            isChan: true,
-            id: 1,
-            name: "wael channel -----------------",
-            avatar: "/image/default.jpg",
+            isChan: false,
+            id: "CatPong's Team",
+            name: "CatPong's Team",
+            avatar: "/image/team.jpg",
           })
         }
         className="left-avatar-button"
@@ -72,13 +163,16 @@ export default function Left() {
         />
         <div className="left-status" />
       </Button>
+
+      {/* CHANNELS PART */}
+      <p className="left-title">Channels</p>
       <Button
         onClick={() =>
           setCurrConv({
-            isChan: false,
-            id: "CatPong's Team",
-            name: "CatPong's Team",
-            avatar: "/image/team.jpg",
+            isChan: true,
+            id: 1,
+            name: "wael channel -----------------",
+            avatar: "/image/default.jpg",
           })
         }
         className="left-avatar-button"
